09673784dfc07e03f7319d3010c61761414cfe6d
hyemi <devham76@gmail.com>
2020-02-05 13:33:24 +0900

    test 수정

--- src/test/java/webservice/springboot2/test/web/IndexControllerTest.java
+++ src/test/java/webservice/springboot2/test/web/IndexControllerTest.java
@@ -17,13 +17,14 @@ public class IndexControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+
     @Test
     public void 메인페이지_로딩() {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
 
     }
 }

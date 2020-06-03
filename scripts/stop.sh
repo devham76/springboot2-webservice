a0dc388375afbe028e6c2e1958f67a6b65c4e87f
Lee Hyemi <devham76@gmail.com>
2020-06-03 15:49:27 +0900

    무중단 배포를 위한 스크립트 파일 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-ABSPATH=$(readlink -f &0)
-ABSDIR=$(dirname $ABSPATH) #현재 stop.sh가 속해 있는 경로
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 
-source ${ABSDIR}/profile.sh # import
+IDLE_PORT=$(find_idle_port)
 
-IDLE_PORT=${find_idle_port}
-
-eco "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
 
 if [ -z ${IDLE_PID} ]
 then
-  echo "> 현재 구동 중인 애플리케이션 없으므로 종료하지 않습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
   kill -15 ${IDLE_PID}
